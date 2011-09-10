17f822b34f8994ead9fac61ae9c73d461bfde8bd
Jack <jack@jack-VirtualBox.(none)>
2011-09-09 23:12:20 -0700

    Initial commit to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperStreet::Application.config.secret_token = 'af8a7542d430e00df997f5498d4a7a099c67bdd812597582e767136f6c381747af37f66f7a32eb06badb4bd2e31b6e1570599aeaedcc3d378b632af4894880f7'
